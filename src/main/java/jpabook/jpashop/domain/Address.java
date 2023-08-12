0bfdfdc8a779818daa9622baa4bd392fe842591c
Lee  Chanho <sma05091@nate.com>
2023-08-12 23:11:32 +0900

    엔티티 클래스 개발 - 회원, 주문, 주문상태, 주문상품, 상품, 배송, 배송상태, 카테고리, 주소 값

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.GeneratedValue;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String Street;
+    private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        Street = street;
+        this.zipcode = zipcode;
+    }
+}
